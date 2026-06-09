d435030c0e0f166bbeefcebe45bb309dfb6716c7
Phillip Gonzales <phillip@phillipgonzales.com>
2026-06-09 15:03:31 -0400

    chore: remove debug test files from src/Services

--- plugin/src/Services/ParagraphFormatter.test.php
+++
@@ -1,5 +0,0 @@
-<?php
-namespace AdrielPartners\WpAudioBuddy\Services;
-final class ParagraphFormatter {
-    public function format(string $s): string { return $s; }
-}
--- plugin/src/Services/test_minimal.php
+++
@@ -1,7 +0,0 @@
-<?php
-declare(strict_types=1);
-namespace AdrielPartners\WpAudioBuddy\Services;
-echo "minimal test\n";
-class MinimalPF {
-    public function format(string $s): string { return $s; }
-}
